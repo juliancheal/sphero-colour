d284fbb695024156a11ff1a5e125425e0f9358e0
Julian Cheal <julian.cheal@gmail.com>
2014-04-28 17:36:51 -0400

    Initial commit

---
+++ app.js
@@ -0,0 +1,39 @@
+var Cylon = require('cylon');
+var colour = 0xFF0066;
+
+var express = require('express'),
+    app = express(),
+    server = require('http').createServer(app),
+    io = require('socket.io').listen(server);
+
+server.listen(8080);
+
+app.get('/', function(req, res){
+    res.sendfile(__dirname + '/index.html');
+});
+
+Cylon.robot({
+  connection: { name: 'sphero', adaptor: 'sphero', port: '/dev/tty.Sphero-BOO-RN-SPP' },
+  device: {name: 'sphero', driver: 'sphero'},
+  
+	work: function(my) {
+		io.sockets.on('connection', function (socket) {
+		  socket.on('color', function (data) {
+				colour = parseInt(data,16);
+		    console.log(colour);
+				
+				my.sphero.setColor(colour);
+				// console.log(my.sphero.getRGB());
+		  });
+		});
+  }
+}).start();
+
+
+// for debuging when no sphero presant
+io.sockets.on('connection', function (socket) {
+  socket.on('color', function (data) {
+		colour = parseInt(data,16);
+    console.log(colour);
+	});	
+});
